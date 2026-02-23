20b865cb0eae5e626060889b49b34e946c1a023f
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2026-02-23 08:09:27 +0700

    add

--- src/Ex2/main.java
+++ src/Ex2/main.java
@@ -1,7 +1,19 @@
 package Ex2;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        ArrayExample arrExample = new ArrayExample();
+        Integer[] arr = arrExample.createRandom();
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = sc.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử của chỉ số thứ " + x + " là: " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
     }
 }
